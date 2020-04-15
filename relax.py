1f9ae57903c927cf4c2618bf845da20294c20e27
TR-Relax <63736122+TR-Relax@users.noreply.github.com>
2020-04-15 22:55:16 +0300

    Add files via upload

---
+++ relax.py
@@ -0,0 +1,12 @@
+deneme = input('\033[1;35m SBomb\n\033[0;32m@@@\033[0;31m@@@@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@\n@@\033[1;31m@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@\033[1;31m@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@\033[1;31m@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@@\033[1;31m@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@@@@@\n@@@\033[1;31m@@@@\033[1;32m@@@@\033[1;31m@@@@@@@@@\033[1;32m@@@@@@@@@@\n@@@@@@@\033[1;31m@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@\n@@@@@@@\033[1;31m@\033[1;32m@@@\033[1;31m@@@@@@@@@\033[1;32m@@@@@@@@@@\n@@@@@@\033[1;31m@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@\n@@\033[1;31m@@@@\033[1;32m@@@@@@@@@@@@@@@@@@@@@@@@\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\033[1;35m Türkiyem Siber Tim\n\033[1;31mSBomb \033[1;32m--》 \033[1;32m The Relax\n\033[1;36mBaşlamak İçin Enterleyin.')
+name = input('\033[1;36mKullanımı \033[1;31m: \033[1;34mTelefonunuzda Telegram Yüklü\n Olması Gerekmektedir. Şifreyi girdikten\n Sonra çıkan bota \033[1;31m/start \033[1;34myazmanız yeterli sonra numarayı girin ve başlıyacak.')
+input('\033[1;39mDevam Etmek İçin Enter')
+password = input('Şifreyi Girin : ')
+pass_main = 'sbomb310819'
+
+if password == password_main:
+    print('Linki Kopyalayıp Google Chrome Yapıştırınız.\nhttps://tr.link/wGJ
+    	
+    	print('\033[1;31mŞifre Yanlış lütfen \033[1;32mhttps://tr.link/FBFy Telegram Kanalından \033[1;31malınız.
+
+
